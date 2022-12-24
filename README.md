2661ad1fc56dc991f27649750d29b92dfdef05c8
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-24 10:17:43 -0700

    Added script to create account for user.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# DAO-Helper-Identity
+######################## Kash Notes
+
+When they sign up on the website they need to create an account on the backend that represents them
+
+1. Least we can do on chain is prove that a wallet owns a profile (V1)
+2. Adding other wallet (ie a reference to an ethereum wallet)
+
+
+1. Need to figure out creating wallet auto
+2. Showing ownership of profile on chain
+
+Using magic link
+
+Wants to get rid of the wallets on site and add them inside of the user profile
+
+
+########################## My Notes
+
+MY GOAL IS TO BE ABLE TO HAND OFF TO FROND END PERSON
+	AKA smart contracts and AlgoSDK scripts (this is my specialization)
+
+First: write a JS script or Python that creates an account and saves the account info in vars
+Second: create smart contract that will be user identity
---
+++ create_account.py
@@ -0,0 +1,76 @@
+# Previously funded account is used to fund new account
+# My address: BSTDOSCCPYN3AZNRGBX3VO4XBEAH2TKCGKYZPMYIIRNYAFHJZGNASJMOEI
+# My private key: cYGCLpViChb078xonSF43x/IUQvdFlI0jPeD30DZCwIMpjdIQn4bsGWxMG+6u5cJAH1NQjKxl7MIRFuAFOnJmg==
+# My passphrase: comfort anxiety nuclear citizen below airport leisure smooth public major rose worth mother stamp tribe bitter medal cotton wink wealth like wagon aware abandon witness
+
+from algosdk import *
+from algosdk.future.transaction import *
+from algosdk.v2client import algod
+from algosdk.future import transaction
+from algosdk import constants
+import json
+import base64
+
+pk_1 = "cYGCLpViChb078xonSF43x/IUQvdFlI0jPeD30DZCwIMpjdIQn4bsGWxMG+6u5cJAH1NQjKxl7MIRFuAFOnJmg=="
+sk_1 = mnemonic.to_private_key("comfort anxiety nuclear citizen below airport leisure smooth public major rose worth mother stamp tribe bitter medal cotton wink wealth like wagon aware abandon witness")
+
+#### ACCOUNT GENERATION ####
+
+# creates an account and prints info
+def generate_algorand_keypair():
+    private_key, address = account.generate_account()
+    new_my_address = format(address)
+    new_my_private_key = format(private_key)
+    new_my_passphrase = format(mnemonic.from_private_key(private_key))
+    return[new_my_address, new_my_private_key, new_my_passphrase]
+
+# funds new account
+def fund_new_acct(private_key, my_address, new_acct_addr):
+    algod_address = "http://localhost:4001"
+    algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    algod_client = algod.AlgodClient(algod_token, algod_address)
+
+    account_info = algod_client.account_info(my_address)
+    print("Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
+
+    # build transaction
+    params = algod_client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = constants.MIN_TXN_FEE 
+    receiver = new_acct_addr
+    note = "Hello World".encode()
+    amount = 1000000
+    unsigned_txn = transaction.PaymentTxn(my_address, params, receiver, amount, None, note)
+
+    # sign transaction
+    signed_txn = unsigned_txn.sign(private_key)
+
+    #submit transaction
+    txid = algod_client.send_transaction(signed_txn)
+    print("Successfully sent transaction with txID: {}".format(txid))
+
+    # wait for confirmation 
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)  
+    except Exception as err:
+        print(err)
+        return
+
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+    print("Decoded note: {}".format(base64.b64decode(
+        confirmed_txn["txn"]["txn"]["note"]).decode()))
+    print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
+    print("Amount transfered: {} microAlgos".format(amount) )    
+    print("Fee: {} microAlgos".format(params.fee) ) 
+
+
+    account_info = algod_client.account_info(my_address)
+    print("Final Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
+
+# CALLING THE FUNCTIONS #
+### This function creates the first new account
+new_address, new_private_key, new_passphrase = generate_algorand_keypair()
+### This function funds the new account with a previously funded testnet account
+fund_new_acct("cYGCLpViChb078xonSF43x/IUQvdFlI0jPeD30DZCwIMpjdIQn4bsGWxMG+6u5cJAH1NQjKxl7MIRFuAFOnJmg==", "BSTDOSCCPYN3AZNRGBX3VO4XBEAH2TKCGKYZPMYIIRNYAFHJZGNASJMOEI", new_address)
