f04b7d072b1366ddebdc9a8cdd78b0359ef08b8e
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-12-25 13:05:04 -0700

    Added get contracts function and building out other helper functions.

--- create_app.py
+++ create_app.py
@@ -83,6 +83,65 @@ fund_new_acct("cYGCLpViChb078xonSF43x/IUQvdFlI0jPeD30DZCwIMpjdIQn4bsGWxMG+6u5cJA
 ######################################
 #### function creates application ####
 
+from typing import Tuple, List
+
+from algosdk.v2client.algod import AlgodClient
+from algosdk.future import transaction
+from algosdk.logic import get_application_address
+from algosdk import account, encoding
+
+from pyteal import compileTeal, Mode
+
+from .account import Account
+from .contracts import approval_program, clear_state_program
+from .util import (
+    waitForTransaction,
+    fullyCompileContract,
+    getAppGlobalState,
+)
+
+from algosdk import account, mnemonic
+
+class Account:
+    """Represents a private key and address for an Algorand account"""
+
+    def __init__(self, privateKey: str) -> None:
+        self.sk = privateKey
+        self.addr = account.address_from_private_key(privateKey)
+
+    def getAddress(self) -> str:
+        return self.addr
+
+    def getPrivateKey(self) -> str:
+        return self.sk
+
+    def getMnemonic(self) -> str:
+        return mnemonic.from_private_key(self.sk)
+
+    @classmethod
+    def FromMnemonic(cls, m: str) -> "Account":
+        return cls(mnemonic.to_private_key(m))
+
+APPROVAL_PROGRAM = b""
+CLEAR_STATE_PROGRAM = b""
+
+def getContracts(client: AlgodClient) -> Tuple[bytes, bytes]:
+    """Get the compiled TEAL contracts for the auction.
+    Args:
+        client: An algod client that has the ability to compile TEAL programs.
+    Returns:
+        A tuple of 2 byte strings. The first is the approval program, and the
+        second is the clear state program.
+    """
+    global APPROVAL_PROGRAM
+    global CLEAR_STATE_PROGRAM
+
+    if len(APPROVAL_PROGRAM) == 0:
+        APPROVAL_PROGRAM = fullyCompileContract(client, approval_program())
+        CLEAR_STATE_PROGRAM = fullyCompileContract(client, clear_state_program())
+
+    return APPROVAL_PROGRAM, CLEAR_STATE_PROGRAM
+
 def createSoulboundApp(
     client: AlgodClient,
     sender: Account,
